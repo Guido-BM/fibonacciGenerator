586e3edb7bf9cc8bb399068f1c4cf9e6f8a61d79
Guido <guidoboninomuelas@gmail.com>
2023-10-12 17:36:24 +0200

    Input mejorado, ahora puedes introducir valores negativos, cero y uno; sin que te devuelva por defecto las dos primeras posiciones

--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,29 @@
-const fib = (n, numFibInicio = [0, 1]) => {
-  if (n <= numFibInicio.length) {
-    return numFibInicio;
+const buttonGenerateFib = document.getElementById("generateFib");
+const inputNumber = document.getElementById("inputNumber");
+const resultNumbers = document.getElementById("resultNumbers");
+
+buttonGenerateFib.addEventListener("click", () => {
+  const n = parseInt(inputNumber.value);
+  const newFibonacciNumber = fib(n);
+  resultNumbers.innerHTML = "";
+  newFibonacciNumber.forEach((num) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = num;
+    resultNumbers.appendChild(listItem);
+  });
+});
+
+const fib = (n) => {
+  if (n < 1) {
+    return [];
   }
-
-  const nextValue =
-    numFibInicio[numFibInicio.length - 1] +
-    numFibInicio[numFibInicio.length - 2];
-  numFibInicio.push(nextValue);
-  return fib(n, numFibInicio);
+  if (n === 1) {
+    return [0];
+  }
+  const numFibInicio = [0, 1];
+  for (let i = 2; i < n; i++) {
+    const nextValue = numFibInicio[i - 1] + numFibInicio[i - 2];
+    numFibInicio.push(nextValue);
+  }
+  return numFibInicio;
 };
-
-const newFibonacci = fib(10);
-console.log(newFibonacci);
